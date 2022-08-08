241c791b014e12e401e3d47de8de2b9263605fa5
Jan Rudolf <jan.rudolf@plus4u.net>
2022-08-08 14:42:12 +0200

    Refactorization of ContextMiddleware, added logger + updated errors.

--- src/middlewares/context-middleware.js
+++ src/middlewares/context-middleware.js
@@ -1,28 +1,31 @@
 import UseCaseEnvironment from '../services/server/use-case-environment.js';
+import UseCaseError from '../services/server/use-case-error.js';
 import RouteRegister from '../services/server/route-register.js';
+import LoggerFactory from '../services/logging/logger-factory.js';
 
-class Error404 extends Error {
+class Error404 extends UseCaseError {
   constructor(req) {
-    super();
-    this.message = 'Handler for request not found.';
-    this.code = 'energy-kit/handlerNotFound';
-    this.params = {
-      url: req.originalUrl,
-    };
-    this.status = 404;
+    super(
+        'Handler for request not found.',
+        'handlerNotFound',
+        {
+          url: req.originalUrl,
+        },
+        404,
+    );
   }
 }
 
-class InvalidMethod extends Error {
+class InvalidMethod extends UseCaseError {
   constructor(req) {
-    super();
-    this.message = 'Handler requested with invalid method.';
-    this.code = 'energy-kit/invalidHandlerMethod';
-    this.params = {
-      url: req.originalUrl,
-      method: req.method,
-    };
-    this.status = 400;
+    super(
+        'Handler requested with invalid method.',
+        'invalidHandlerMethod',
+        {
+          url: req.originalUrl,
+          method: req.method,
+        },
+    );
   }
 }
 
@@ -30,10 +33,13 @@ class ContextMiddleware {
   constructor() {
     // this middlewares must be first at all costs
     this.ORDER = -999;
+    this.logger = LoggerFactory.create('Context.Middleware');
   }
 
   async process(req, res, next) {
-    console.log('Incoming req', req.path);
+    if (this.logger.isDebugEnabled()) {
+      this.logger.debug(`Path: ${req.path} Method: ${req.method}`);
+    }
 
     // prepare ucEnv to collect any custom attributes (mapping, dtoIn, session so far)
     const ucEnv = new UseCaseEnvironment(req, res);
