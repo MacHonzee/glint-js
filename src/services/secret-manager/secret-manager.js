db985a3f422eba49207bdff1b87b56d210e57114
Jan Rudolf <jan.rudolf@plus4u.net>
2022-10-10 21:15:57 +0200

    Added extra debugging for SecretManager.

--- src/services/secret-manager/secret-manager.js
+++ src/services/secret-manager/secret-manager.js
@@ -1,16 +1,28 @@
 import {SecretManagerServiceClient} from '@google-cloud/secret-manager';
 import Config from '../utils/config.js';
+import LoggerFactory from '../logging/logger-factory.js';
 
 class SecretManager {
-  gcpProject = Config.get('GOOGLE_CLOUD_PROJECT');
-  client = new SecretManagerServiceClient({
-    projectId: this.gcpProject, // necessary for localhost
-    keyFilename: Config.get('GCP_KEY_FILENAME'), // necessary for localhost
-  });
-  _cachedSecrets = {};
+  constructor() {
+    this.gcpProject = Config.get('GOOGLE_CLOUD_PROJECT');
+    const gcpKeyFilename = Config.get('GCP_KEY_FILENAME');
+
+    this._logger = LoggerFactory.create('Server.SecretManager');
+    this._logger.debug(`Initializing SecretManager for project with id '${this.gcpProject}'`);
+    this._logger.debug(`Key path for SecretManager is '${gcpKeyFilename}'`);
+
+    this.client = new SecretManagerServiceClient({
+      projectId: this.gcpProject, // necessary for localhost
+      keyFilename: gcpKeyFilename, // necessary for localhost
+    });
+    this._cachedSecrets = {};
+  }
 
   async get(secretName, version = 'latest') {
+    this._logger.debug(`Getting secret with name '${secretName}'`);
     const secretPath = this.getSecretPath(secretName, version);
+    this._logger.debug(`Secret with name '${secretName}' has path '${secretPath}'`);
+
     if (this._cachedSecrets[secretPath]) return this._cachedSecrets[secretPath];
 
     const [secret] = await this.client.accessSecretVersion({
