6cb2cc4a942ab4ef956f7c812861baad8ce82e9f
Jan Rudolf <jan.rudolf@plus4u.net>
2022-11-25 08:56:58 +0100

    Changed SecretManager to be lazily initiated after first attempt to get value or secret path.

--- src/services/secret-manager/secret-manager.js
+++ src/services/secret-manager/secret-manager.js
@@ -5,20 +5,14 @@ import LoggerFactory from '../logging/logger-factory.js';
 class SecretManager {
   constructor() {
     this._logger = LoggerFactory.create('Service.SecretManager');
-
-    this.disabled = Config.get('SECRET_MANAGER_DISABLED', Boolean);
-    if (!this.disabled) {
-      this._init();
-    } else {
-      this._logger.info('SecretManager is disabled.');
-    }
+    this._active = false;
   }
 
   async get(secretName, version = 'latest') {
-    if (this.disabled) throw new Error('SecretManager is disabled, cannot get secret.');
+    if (!this._active) await this._init(); // lazy initialization during first call
 
     this._logger.debug(`Getting secret with name '${secretName}'`);
-    const secretPath = this.getSecretPath(secretName, version);
+    const secretPath = await this.getSecretPath(secretName, version);
     this._logger.debug(`Secret with name '${secretName}' has path '${secretPath}'`);
 
     if (this._cachedSecrets[secretPath]) return this._cachedSecrets[secretPath];
@@ -33,12 +27,14 @@ class SecretManager {
     return secretContent;
   }
 
-  getSecretPath(secretName, version = 'latest') {
+  async getSecretPath(secretName, version = 'latest') {
+    if (!this._active) await this._init(); // lazy initialization during first call
+
     return `projects/${this.gcpProject}/secrets/${secretName}/versions/${version}`;
   }
 
   _init() {
-    this.gcpProject = Config.get('GOOGLE_CLOUD_PROJECT');
+    this.gcpProject = Config.mustGet('GOOGLE_CLOUD_PROJECT');
     const gcpKeyFilename = Config.get('GCP_KEY_FILENAME');
 
     this._logger.debug(`Initializing SecretManager for project with id '${this.gcpProject}'`);
@@ -49,6 +45,7 @@ class SecretManager {
       keyFilename: gcpKeyFilename, // necessary for localhost
     });
     this._cachedSecrets = {};
+    this._active = true;
   }
 }
 
