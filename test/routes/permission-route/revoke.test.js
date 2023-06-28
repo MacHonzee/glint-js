ab1e25ea913db1ef5cd743a4b1e54eb357b1ba39
Jan Rudolf <jan.rudolf@plus4u.net>
2023-06-28 11:15:00 +0200

    Added tests for permission/revoke.

---
+++ test/routes/permission-route/revoke.test.js
@@ -0,0 +1,55 @@
+import { describe, it, expect, beforeAll, beforeEach } from "@jest/globals";
+import { TestService, TestUsers } from "../../test-utils/index.js";
+import { AuthorizationService } from "../../../src/index.js";
+import PermissionRoute from "../../../src/routes/permission-route.js";
+
+const USER = {
+  username: "userforrevoke@test.com",
+  password: "123456",
+  confirmPassword: "123456",
+  firstName: "Permission",
+  lastName: "Grant",
+  email: "userforrevoke@test.com",
+  language: "en",
+};
+const ROLES = ["Authority", "Technician"];
+
+describe("permission/revoke", () => {
+  let testUser;
+  beforeAll(async () => {
+    testUser = await TestUsers.registerUser(USER);
+    USER.id = testUser.user.id;
+  });
+
+  beforeEach(async () => {
+    await TestUsers.grantPermissions("userforrevoke", ROLES);
+  });
+
+  it("should return success", async () => {
+    const revoke = {
+      user: testUser.user.username,
+      role: "Technician",
+    };
+    const ucEnv = await TestService.getUcEnv("permission/revoke", revoke);
+    const dtoOut = await PermissionRoute.revoke(ucEnv);
+
+    const userRoles = await AuthorizationService.getUserRoles(revoke.user);
+
+    expect(dtoOut.revoked).toBe(revoke.role);
+    expect(userRoles).toMatchObject(ROLES.filter((role) => role !== revoke.role));
+  });
+
+  it("should revoke all permissions", async () => {
+    const revoke = {
+      user: testUser.user.username,
+      all: true,
+    };
+    const ucEnv = await TestService.getUcEnv("permission/revoke", revoke);
+    const dtoOut = await PermissionRoute.revoke(ucEnv);
+
+    const userRoles = await AuthorizationService.getUserRoles(revoke.user);
+
+    expect(dtoOut.revoked).toBe("all");
+    expect(userRoles).toMatchObject([]);
+  });
+});
